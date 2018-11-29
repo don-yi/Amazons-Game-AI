beb2739c3db22042cf8b850eaec6aa7077985aa7
Dennis Lee <dennis.donguk.lee@gmail.com>
2018-11-29 13:19:21 -0800

    Add read from file function and re factor into a class.

--- game-of-the-amazons_ai/main.cpp
+++ game-of-the-amazons_ai/main.cpp
@@ -1,44 +1,99 @@
 #include <array>
 #include <iostream>
+#include <fstream>
+#include <cassert>
+#include <string>
 
 #define ROW_COUNT 10
 #define COL_COUNT 10
 
-// Not used.
-// Make a new board.
-std::array<std::array<int, COL_COUNT>, ROW_COUNT> generate_new_board(void)
+class amazons
 {
-	//std::array<std::array<int, COL_COUNT>, ROW_COUNT> newBoard{};
-
-	//// Assign player values at the default starting positions.
-	//newBoard[0][3] = 1;
-	//newBoard[0][6] = 1;
-	//newBoard[3][0] = 1;
-	//newBoard[3][9] = 1;
-
-	//newBoard[6][0] = 2;
-	//newBoard[6][9] = 2;
-	//newBoard[9][3] = 2;
-	//newBoard[9][6] = 2;
-
-	// temp testing board
-	std::array<std::array<int, COL_COUNT>, ROW_COUNT> newBoard;
-
-	for (auto&& eachCol : newBoard)
+public:
+	enum coord_status
 	{
-		eachCol.fill(3);
+		blank,
+		black,
+		white,
+		killed
+	};
+
+	// Not used.
+	// Make a new board.
+	std::array<std::array<int, COL_COUNT>, ROW_COUNT> generate_new_board(void)
+	{
+		//std::array<std::array<int, COL_COUNT>, ROW_COUNT> newBoard{};
+
+		//// Assign player values at the default starting positions.
+		//newBoard[0][3] = 1;
+		//newBoard[0][6] = 1;
+		//newBoard[3][0] = 1;
+		//newBoard[3][9] = 1;
+
+		//newBoard[6][0] = 2;
+		//newBoard[6][9] = 2;
+		//newBoard[9][3] = 2;
+		//newBoard[9][6] = 2;
+
+		// temp testing board
+		std::array<std::array<int, COL_COUNT>, ROW_COUNT> newBoard{};
+
+		for (auto&& eachCol : newBoard)
+		{
+			eachCol.fill(3);
+		}
+
+		// Assign testing values.
+		newBoard[7][6] = 1;
+		newBoard[8][6] = 0;
+		newBoard[9][6] = 0;
+
+		newBoard[7][7] = 0;
+		newBoard[9][7] = 2;
+
+		return newBoard;
 	}
 
-	// Assign testing values.
-	newBoard[7][6] = 1;
-	newBoard[8][6] = 0;
-	newBoard[9][6] = 0;
+	// Read a board from a text file and convert it into a two dimensional array.
+	std::array<std::array<int, COL_COUNT>, ROW_COUNT> read_from_file(
+		const std::string& fileName
+	)
+	{
+		std::ifstream file(fileName);
 
-	newBoard[7][7] = 0;
-	newBoard[9][7] = 2;
+		// Check the file was opened.
+		const auto open = file.is_open();
+		assert(open);
 
-	return newBoard;
-}
+		std::array<std::array<int, COL_COUNT>, ROW_COUNT> board{};
+		std::string line;
+
+		// Extract from the file and assign to a board.
+		for (size_t x = 0; x < COL_COUNT; ++x)
+		{
+			std::getline(file, line);
+			size_t y = 0;
+			for (auto ch : line)
+			{
+				if (ch == ' ')
+				{
+					continue;
+				}
+				else
+				{
+					board[x][y++] = atoi(&ch);
+				}
+			}
+		}
+
+		// Close the file and return.
+		file.close();
+		return board;
+	}
+
+private:
+
+};
 
 // Print the board in human-readable form.
 void print_board(
@@ -51,16 +106,16 @@ void print_board(
 		{
 			switch (board[x][y])
 			{
-			case 0:
-				std::cout << "_";
+			case amazons::black:
+				std::cout << '_';
 				break;
-			case 3:
-				std::cout << "X";
+			case amazons::killed:
+				std::cout << 'X';
 				break;
 			default:
 				std::cout << board[x][y];
 			}
-			std::cout << " ";
+			std::cout << ' ';
 		}
 		std::cout << std::endl;
 	}
@@ -68,8 +123,9 @@ void print_board(
 
 int main(void)
 {
-	auto const& tmpBoard = generate_new_board();
-	print_board(tmpBoard);
+	//auto const& tmpBoard = generate_new_board();
+	//auto const& tmpBoard = read_from_file("TestBoard.txt");
+	//print_board(tmpBoard);
 
 	std::getchar();
 
