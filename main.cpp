4a8b60e33c583ac443b705538564663e030bbee5
Dennis Lee <dennis.donguk.lee@gmail.com>
2018-11-29 01:51:00 -0800

    Assign player values at the default starting positions in generate board functiona and add print board function.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,49 @@
 #include <array>
+#include <iostream>
 
 #define ROW_COUNT 10
 #define COL_COUNT 10
 
-// Create new board.
-std::array<std::array<int, COL_COUNT>, ROW_COUNT> create_new_board(void)
+// Make a new board.
+std::array<std::array<int, COL_COUNT>, ROW_COUNT> generate_new_board(void)
 {
-	//const auto newBoard = new int*[ROW_COUNT];
-	//for (auto i = 0; i < ROW_COUNT; ++i)
-	//{
-	//	newBoard[i] = new int[COL_COUNT];
-	//}
+	std::array<std::array<int, COL_COUNT>, ROW_COUNT> newBoard{};
 
-	const std::array<std::array<int, COL_COUNT>, ROW_COUNT> newBoard{};
+	// Assign player values at the default starting positions.
+	newBoard[0][3] = 1;
+	newBoard[0][6] = 1;
+	newBoard[3][0] = 1;
+	newBoard[3][9] = 1;
+
+	newBoard[6][0] = 2;
+	newBoard[6][9] = 2;
+	newBoard[9][3] = 2;
+	newBoard[9][6] = 2;
 
 	return newBoard;
 }
 
+// Print the board.
+void print_board(
+	std::array<std::array<int, COL_COUNT>, ROW_COUNT> const& board
+)
+{
+	for (auto x = 0; x < ROW_COUNT; ++x)
+	{
+		for (auto y = 0; y < COL_COUNT; ++y)
+		{
+			std::cout << board[x][y] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
 int main(void)
 {
+	auto const& tmpBoard = generate_new_board();
+	print_board(tmpBoard);
+
+	std::getchar();
 
 	return 0;
 }
