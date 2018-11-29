3067d29560b8ec7a5d27aa800b813e443435e85d
Dennis Lee <dennis.donguk.lee@gmail.com>
2018-11-29 01:23:05 -0800

    Add create new board function.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
+#include <array>
+
+#define ROW_COUNT 10
+#define COL_COUNT 10
+
+// Create new board.
+std::array<std::array<int, COL_COUNT>, ROW_COUNT> create_new_board(void)
+{
+	//const auto newBoard = new int*[ROW_COUNT];
+	//for (auto i = 0; i < ROW_COUNT; ++i)
+	//{
+	//	newBoard[i] = new int[COL_COUNT];
+	//}
+
+	const std::array<std::array<int, COL_COUNT>, ROW_COUNT> newBoard{};
+
+	return newBoard;
+}
+
 int main(void)
 {
 
