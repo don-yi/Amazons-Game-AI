a0213321d6ebe06f02a79c4831c075dfa4bba963
Dennis Lee <dennis.donguk.lee@gmail.com>
2018-12-04 04:01:16 -0800

    Add input safety handling loops.

--- game-of-the-amazons_ai/main.cpp
+++ game-of-the-amazons_ai/main.cpp
@@ -36,6 +36,7 @@ void print_help()
 {
 	std::cout << "h for help (this page)" << std::endl;
 	std::cout << "l (lowercase L) for listing moves" << std::endl;
+	std::cout << "m to input a move" << std::endl;
 	std::cout << "Making move: Target, Move & Shoot Coordinates" << std::endl;
 	std::cout
 		<< "ie. \"Move a player at (7, 6) to (7, 7) and shoot to (7, 6)\""
@@ -62,67 +63,117 @@ void print_moves(const std::list<move> li)
 
 int main()
 {
-	// Input board.
-	std::cout << "Use StartBoard.txt/TestBoard.txt?" << std::endl;
-	std::cout << "type s for sb" << std::endl;
-	std::cout << "type t for tb" << std::endl;
-
-	amazons a1;
-	const auto boardInput = getchar();
-	if (boardInput == 's')
+	while (true)
 	{
-		a1.read_from_file("StartBoard.txt");
+		amazons a1;
+
+		// Input board.
+		while (true)
+		{
+			std::cout << std::endl;
+			std::cout << "Use StartBoard.txt/TestBoard.txt?" << std::endl;
+			std::cout << "type s for sb" << std::endl;
+			std::cout << "type t for tb" << std::endl;
+
+			const auto boardInput = getchar();
+			if (boardInput == 's')
+			{
+				a1.read_from_file("StartBoard.txt");
+				std::cout << std::endl;
+				print_board(a1.prevMat);
+				break;
+			}
+			else if (boardInput == 't')
+			{
+				a1.read_from_file("TestBoard.txt");
+				std::cout << std::endl;
+				print_board(a1.prevMat);
+				break;
+			}
+		}
+
+		// Input player.
+		while (true)
+		{
+			std::cout << std::endl;
+			std::cout << "Use black(1)/white(2)?" << std::endl;
+			std::cout << "type b or w" << std::endl;
+
+			const auto playerInput = getchar();
+			amazons::coord_status player;
+			amazons::coord_status opponent;
+			if (playerInput == 'b')
+			{
+				player = amazons::black;
+				opponent = amazons::white;
+				break;
+			}
+			else if (playerInput == 'w')
+			{
+				player = amazons::white;
+				opponent = amazons::black;
+				break;
+			}
+		}
+
+		// Input the turn.
+		while (true)
+		{
+			std::cout << std::endl;
+			std::cout << "Go first/second when starting?" << std::endl;
+			std::cout << "type 1 or 2" << std::endl;
+
+			const auto turnInput = getchar();
+			bool isFirstTurn;
+			if (turnInput == '1')
+			{
+				isFirstTurn = true;
+				break;
+			}
+			else if (turnInput == '2')
+			{
+				isFirstTurn = false;
+				break;
+			}
+		}
+
+		// Input AI.
+		while (true)
+		{
+			std::cout << std::endl;
+			std::cout << "AI?" << std::endl;
+			std::cout << "1 for min/max" << std::endl;
+			std::cout << "2 for max/min" << std::endl;
+
+			const auto AIInput = getchar();
+			amazons::AI ai;
+			if (AIInput == '1')
+			{
+				ai = amazons::minMax;
+				break;
+			}
+			else if (AIInput == '2')
+			{
+				ai = amazons::maxMin;
+				break;
+			}
+		}
+
+		//// game loop
+		//auto plmvli = amazons::list_all_moves(player, a1.prevMat);
+		//auto oppomvli = amazons::list_all_moves(player, a1.prevMat);
+		//while ()
+		//{
+
+		//}
+
+
+		//const auto tmp = a1.next_move(amazons::white, amazons::maxMin);
+
+		//print_moves(a1.list_moves(amazons::black));
+		//print_moves(a1.list_moves(amazons::white));
+
 	}
-	else if (boardInput == 't')
-	{
-		a1.read_from_file("TestBoard.txt");
-	}
-	std::cout << std::endl;
-	print_board(a1.prevMat);
-
-	// Input player.
-	std::cout << std::endl;
-	std::cout << "Use black(1)/white(2)?" << std::endl;
-	std::cout << "type b or w" << std::endl;
-
-	const auto playerInput = getchar();
-	amazons::coord_status player;
-	if (boardInput == 'b')
-	{
-		player = amazons::black;
-	}
-	else if (boardInput == 'w')
-	{
-		player = amazons::white;
-	}
-	std::cout << std::endl;
-
-	// Input AI.
-	std::cout << std::endl;
-	std::cout << "AI?" << std::endl;
-	std::cout << "1 for min/max" << std::endl;
-	std::cout << "2 for max/min" << std::endl;
-
-	const auto AIInput = getchar();
-	amazons::AI ai;
-	if (AIInput == '1')
-	{
-		ai = amazons::minMax;
-	}
-	else if (AIInput == '2')
-	{
-		ai = amazons::maxMin;
-	}
-	std::cout << std::endl;
-
-
-
-	//const auto tmp = a1.next_move(amazons::white, amazons::maxMin);
-
-	//print_moves(a1.list_moves(amazons::black));
-	//print_moves(a1.list_moves(amazons::white));
-
-	std::getchar();
 
 	return 0;
 }
